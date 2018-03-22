7783f71fc3b4e36d8733fa78190d568279bedf18
Nathan Kear <n.j.kear@gmail.com>
2018-03-22 22:30:39 +1300

    Add weather charts and map of location

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = '493738b86f9b262c9f0cb8a9f3910b9f';
+import axios from 'axios';
 
+const API_KEY = '493738b86f9b262c9f0cb8a9f3910b9f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},nz`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
---
+++ src/components/chart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines
+                data={props.data}
+                width={180} 
+                height={120}>
+                <SparklinesLine color={props.colour} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
+        </div>
+    );
+}
+
+function average(data) {
+    return _.round(_.sum(data)/data.length);
+}
